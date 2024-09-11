c6367b1733b5f997fe60599db8cf146817d4a3fa
miguelhuayhua <miguelhuayhuac2@gmail.com>
2024-09-11 16:04:54 -0400

    carpetas

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
+const fs = require('fs'); // Importa fs para manejar la creación de carpetas
 const app = express();
 
 // Middleware para procesar datos del formulario que no son archivos
 app.use(express.urlencoded({ extended: true }));
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         const { modo } = req.headers;
         const { tipo } = req.headers;
-        const folderPath = `uploads/${tipo}/${modo}`;
+        const folderPath = path.join(__dirname, 'uploads', tipo, modo); // Define la ruta absoluta
+
+        // Verifica si la carpeta existe, si no, la crea
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true }); // Crea las carpetas de manera recursiva
+        }
+
         cb(null, folderPath); // Asigna la carpeta destino
     },
     filename: (req, file, cb) => {
@@ -18,7 +26,9 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + sufijo + ext); // Nombre del archivo
     }
 });
+
 const upload = multer({ storage });
+
 // Ruta para manejar la subida de archivos y campos
 app.post('/upload', upload.single('file'), (req, res) => {
     if (req.file) {
@@ -35,10 +45,13 @@ app.post('/upload', upload.single('file'), (req, res) => {
         return res.json({
             error: true,
             path: ''
-        })
+        });
     }
 });
+
+// Servir archivos estáticos desde la carpeta uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // Inicia el servidor
 app.listen(process.env.PORT || 4000, () => {
     console.log('A la escucha');
